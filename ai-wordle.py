c95b82586f76a01af57871fb184bd1b80baeec0e
aidandevaal <aidan_de_vaal@sfu.ca>
2024-01-26 15:25:27 -0800

    lots of filtering - start of ai

--- ai-wordle.py
+++ ai-wordle.py
@@ -8,11 +8,12 @@ myDict = open("wdict.txt","r")
 wordList = [word for word in myDict]
 
 targetIndex = random.randint(0,len(wordList)-1)
-target = wordList[targetIndex]
+#target = wordList[targetIndex]
 
-starting = "trail"
+target = "trade"
+starting = "trial"
 
-found = "....."
+found = []
 foundIndices = []
 
 container = []
@@ -22,33 +23,88 @@ deadLetters = []
 possibilities = []
 
 
-for index,letter in enumerate(starting):
-    if letter == target[index]:
-        found[index] = letter
-        foundIndices.append(index)
-    elif letter in target:
-        container.append(letter)
-    else:
-        deadLetters.append(letter)
+#for index,letter in enumerate(starting):
+#    if(letter == target[index]):
+#        found.insert(index, letter)
+#        foundIndices.append(index)
+#        container.append(letter)
+#    elif(letter in target):
+#        container.append(letter)
+#    else:
+#        deadLetters.append(letter)
 
+def start():
+    choice = input("Enter a string for the AI to crack: ")
+    end(choice)
+    gameState(choice)
 
-def gameState():
-    possibilities = []
+def end(guess):
+    if(guess == target):
+        print("ez")
+        quit()
+
+def xor(a, b):
+    return ((a and not b) or (not a and b))
+
+def gameState(guess):
+    for index,letter in enumerate(guess):
+        if(letter == target[index]):
+            found.insert(index, letter)
+            foundIndices.append(index)
+            container.append(letter)
+        elif(letter in target):
+            container.append(letter)
+        else:
+            deadLetters.append(letter)
+    if(guess in possibilities):
+        possibilities.remove(guess)
+    wordCheck()
+
+def wordCheck():
     for word in wordList:
+        count = 0
         for num in foundIndices:
-            if found[num] == target[num]:
-                for letter in container:
-                    if letter in target:
-                        for letter in deadLetters:
-                            if letter in target:
-                                break
-                            else:
-                                possibilities[len(possibilities)] = word
+            if((len(found) > 0) and (word[foundIndices[num]] == target[foundIndices[num]])):
+                count+=1
+        if(count == len(foundIndices)):
+            possibilities.append(word)
 
-gameState()
+    toRemove = []
 
-for word in possibilities:
-    print(word)
+    for word in possibilities:
+        count = 0
+        for letter in container:
+            if(letter not in word):
+                toRemove.append(word)
+
+    for word in toRemove:
+        possibilities.remove(word)
+
+def AI():
+    likelihoods = {}
+    for word in wordList:
+        for letter in word:
+            
+            likelihoods.update(letter)
+
+
+def main():
+    start()
+    for word in possibilities:
+        print(word)
+
+    print("Container: ")
+
+    for letter in container:
+        print(letter)
+
+    print("Found: ")
+    for letter in found:
+        print(letter)
+
+
+if __name__ == '__main__':
+    main()
             
 
 
